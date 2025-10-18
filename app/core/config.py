12bbfde3d5f1e313552648bd9c97e614b7eea31a
Haas <t.haas@kunze-ritter.de>
2025-10-18 17:32:12 +0200

    fix: Allow extra fields in Settings for docker-compose env vars

--- app/core/config.py
+++ app/core/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     """Application Settings"""
 
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", case_sensitive=False
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore"
     )
 
     # Application
